e9ef80fbf627bf3450282e453743cbb4c0359835
M. Kamran <kamran1630>
2021-10-01 17:39:16 +0500

    by kamran

--- resources/views/reports/netsale_report.blade.php
+++ resources/views/reports/netsale_report.blade.php
@@ -20,7 +20,7 @@
           <th scope="col" style="border-right: 1px solid black;text-align: center;">#</th>
           <th scope="col" style="border-right: 1px solid black;text-align: center;">OrderId</th>
           <th scope="col" style="border-right: 1px solid black;text-align: center;">Table No</th>
-          <th scope="col" style="border-right: 1px solid black;text-align: center;">Total Item/Kg</th>
+          <th scope="col" style="border-right: 1px solid black;text-align: center;">Items</th>
           <th scope="col" style="border-right: 1px solid black;text-align: center;">Total Price</th>
           <th scope="col" style="border-right: 1px solid black;text-align: center;">Date</th>
         </tr>
@@ -31,13 +31,19 @@
           @foreach($record as $key => $value)
             @php
               $mm = $menus->where('id',$value->cat_id)->first();
+              $order_detail = json_decode($value->order_detail,true);
               $tt = $tt + $value->total_price;
             @endphp
             <tr>
               <td style="border-right: 1px solid black; border-bottom: 1px solid black; padding:5px;text-align: center;">{{ ++$key }}</td>
               <td style="border-right: 1px solid black; border-bottom: 1px solid black; padding:5px;text-align: center;">{{ $value->id }}</td>
               <td style="border-right: 1px solid black; border-bottom: 1px solid black; padding:5px;text-align: center;">{{ $value->table_no }}</td>
-              <td style="border-right: 1px solid black; border-bottom: 1px solid black; padding:5px;text-align: center;">{{ $value->qty }}</td>
+              <td style="border-right: 1px solid black; border-bottom: 1px solid black; padding:5px;text-align: center;">
+                @forelse($order_detail as $qwe)
+                  {{ $qwe['name']." x ".$qwe['qty']."  " }}
+                @empty
+                @endforelse
+              </td>
               <td style="border-right: 1px solid black; border-bottom: 1px solid black; padding:5px;text-align: center;">{{ number_format($value->total_price) }}</td>
               <td style="border-right: 1px solid black; border-bottom: 1px solid black; padding:5px;text-align: center;">{{ (!empty($value->date))?date('d/m/Y' , strtotime($value->date)):"" }}</td>
             </tr>
